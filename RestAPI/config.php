b6c35eaa0fff31d6b42e262d460f6dd373b8584c
Filipe Santos <fbnsantos@fbnsantos.com>
2024-09-06 22:43:22 +0100

    pass

--- RestAPI/config.php
+++ RestAPI/config.php
@@ -3,6 +3,6 @@
 $database_host = '{{DB_HOST}}';  // Placeholder
 $database_user = '{{DB_USER}}';  // Placeholder
 $database_password = '{{DB_PASSWORD}}';  // Placeholder
-
+$dbname = 'pikachuPM';
 
 ?>
--- RestAPI/index.php
+++ RestAPI/index.php
@@ -5,4 +5,19 @@ include 'config.php';
 
 // Faz echo da variável definida em config.php
 echo "O host do banco de dados é: " . $database_host;
+
+try {
+    // Tentar criar uma nova conexão PDO
+    $pdo = new PDO("mysql:host=$database_host;dbname=$dbname", $database_user, $database_password);
+    
+    // Definir o modo de erro PDO para exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Teste de conexão bem-sucedida
+    echo "Conexão com o banco de dados bem-sucedida!";
+    
+} catch (PDOException $e) {
+    // Se houver erro de conexão, ele será capturado aqui
+    echo "Erro na conexão com o banco de dados: " . $e->getMessage();
+}
 ?>

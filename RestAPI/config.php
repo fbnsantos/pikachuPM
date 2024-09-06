8e87c7cf5edfc648a466a2d2dcdc5d62c2c2ed00
Filipe Santos <fbnsantos@fbnsantos.com>
2024-09-06 22:33:40 +0100

    versaorestapi

---
+++ RestAPI/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$database_host = '{{DB_HOST}}';  // Placeholder
+$database_user = '{{DB_USER}}';  // Placeholder
+$database_password = '{{DB_PASSWORD}}';  // Placeholder
+
+
+?>
---
+++ RestAPI/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Inclui o arquivo config.php
+include 'config.php';
+
+// Faz echo da variável definida em config.php
+echo "O host do banco de dados é: " . $database_host;
+?>

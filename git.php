ce630496adc7ca38beb3a2d006ee30bd76b39663
Filipe Neves dos Santos <fbnsantos@fbnsantos.com>
2024-12-06 23:44:31 +0000

    sd

--- git.php
+++ git.php
@@ -3,8 +3,9 @@
 $currentUser = get_current_user();
 echo "<p>Current User: " . $currentUser . "</p>";
 
-$output = shell_exec("git config --global --add safe.directory /var/www/html/pikachu/pikachuPM");
+$output = shell_exec("git config --global --add safe.directory /var/www/html/pikachu/pikachuPM 2>&1");
 echo "<pre>$output</pre>";
+
 
 // Executar o comando 'git pull'
 $output = shell_exec('git pull 2>&1');

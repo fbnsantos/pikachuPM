a04b37e91b46b43c04fa0819d0977f00f265f4f8
Filipe Neves dos Santos <fbnsantos@fbnsantos.com>
2024-12-06 23:53:36 +0000

    s

--- git.php
+++ git.php
@@ -3,6 +3,9 @@
 $currentUser = get_current_user();
 echo "<p>Current User: " . $currentUser . "</p>";
 
+$output = shell_exec("whoami");
+echo "<pre>$output</pre>";
+
 $output = shell_exec("git config --global --add safe.directory /var/www/html/pikachu/pikachuPM 2>&1");
 echo "<pre>$output</pre>";
 

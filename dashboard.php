7b91a6f57b289108e5aa236c7a4b88b7af14d317
fbnsantos <fbnsantos@fbnsantos.com>
2025-04-21 22:07:39 +0100

    test

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,2 @@
-<?php
-session_start();
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit;
-}
-?>
-
-<h1>Olá, <?= htmlspecialchars($_SESSION['username']) ?>!</h1>
-<p>Este é o painel depois do login via Redmine.</p>
-<a href="logout.php">Sair</a>
+<h2>Painel Principal</h2>
+<p>Este é o conteúdo do painel principal.</p>

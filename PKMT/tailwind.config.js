aad3fb410a55b2fadcd37ebd0fe3367b202a9f5b
fbnsantos <fbnsantos@fbnsantos.com>
2025-05-04 23:19:54 +0100

    xdx

--- PKMT/tailwind.config.js
+++ PKMT/tailwind.config.js
@@ -1,11 +1,5 @@
-// tailwind.config.js
-export default {
-    content: [
-      './index.html',
-      './src/**/*.{js,jsx}',
-    ],
-    theme: {
-      extend: {},
-    },
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,jsx}'],
+    theme: { extend: {} },
     plugins: [],
-  }
+  };

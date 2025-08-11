a3e9f5af34755974ccbc7f3658fb5de3dc46bd34
fbnsantos <fbnsantos@fbnsantos.com>
2025-08-11 18:40:57 +0100

    dsf

--- tabs/bomlist/database/database.php
+++ tabs/bomlist/database/database.php
@@ -15,7 +15,7 @@ function connectDB(){
 
     try {
          echo "1";
-        $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
+        $pdo = new PDO("mysql:host=127.0.0.1;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
         echo "2";
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 

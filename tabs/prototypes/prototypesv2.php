39c406bad4d3ee78d22a44d98b47aaa222315d14
Filipe Santos <fbnsantos@fbnsantos.com>
2026-08-07 12:57:53 +0100

    features added

--- tabs/prototypes/prototypesv2.php
+++ tabs/prototypes/prototypesv2.php
@@ -1226,7 +1226,10 @@ if ($filterParticipate && $currentUserId) {
 $whereClause = !empty($whereConditions) ? 'WHERE ' . implode(' OR ', $whereConditions) : '';
 
 if ($filterType) {
-    $whereClause = $whereClause ? $whereClause . ' AND p.prototype_type = ?' : 'WHERE p.prototype_type = ?';
+    // inclui pais que têm o tipo E todos os filhos directos desses pais
+    $typeCondition = '(p.prototype_type = ? OR p.parent_id IN (SELECT id FROM prototypes WHERE prototype_type = ?))';
+    $whereClause = $whereClause ? $whereClause . ' AND ' . $typeCondition : 'WHERE ' . $typeCondition;
+    $params[] = $filterType;
     $params[] = $filterType;
 }
 
